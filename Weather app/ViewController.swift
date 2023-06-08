49b27998c3b8e06bdee6f7b35ff822ff78993063
Jakub Sukiennik <lordquick@icloud.com>
2023-06-08 12:58:07 +0200

    upload wether from html

--- Weather app/ViewController.swift
+++ Weather app/ViewController.swift
@@ -9,11 +9,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var city: UITextField!
+    
+    @IBOutlet weak var weather: UILabel!
+    
+    @IBAction func show(_ sender: Any) {
+        
+        let url = URL(string: "https://www.weather-forecast.com/locations/"+city.text!+"/forecasts/latest")
+        var urlError = false
+        var weath = ""
+        if url != nil {
+            
+            
+            let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+                
+                if error == nil
+                {
+                    var urlcontetn = NSString(data: data!, encoding: String.Encoding.utf8.rawValue) as NSString?
+                  
+                    
+                    var urlarray = urlcontetn?.components(separatedBy: "<span class=\"phrase\">")
+                    
+                    if urlarray!.count > 0
+                    {
+                        var weatherarray = urlarray![1].components(separatedBy: "</span")
+                         weath = weatherarray[0] as String
+                        
+                    }
+                    else{
+                        urlError = true
+                    }
+                    
+                }else{
+                    urlError = true
+                }
+                
+                if urlError == true
+                {
+                    self.showError()
+                }else
+                {
+                    self.weather.text = weath                 }
+                
+            }
+            task.resume()
+        }else {
+            
+            showError()
+        }
+        
+    }
+    func showError()
+    {
+        weather.text = "Brak możliwości znalezienia pogody dla " + city.text! + "Try again"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+        
+            
+            
+            
+        }
 }
+
+
+
 
